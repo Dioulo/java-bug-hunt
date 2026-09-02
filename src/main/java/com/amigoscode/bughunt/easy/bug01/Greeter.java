0923577cc8a26a51bc9ae0d3e28d7f062a615df5
tokyo <dioulotony@gmail.com>
2026-09-02 14:52:06 -0400

    solve bug01

--- src/main/java/com/amigoscode/bughunt/easy/bug01/Greeter.java
+++ src/main/java/com/amigoscode/bughunt/easy/bug01/Greeter.java
@@ -3,6 +3,9 @@ package com.amigoscode.bughunt.easy.bug01;
 public class Greeter {
 
     public String greet(String name) {
+        if(name == null) {
+            name = "guest";
+        }
         return "Hello, " + name.toUpperCase() + "!";
     }
 }
