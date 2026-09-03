59107a4ac1cd6e343984706511e91fc4d1f177b1
tokyo <dioulotony@gmail.com>
2026-09-02 20:00:06 -0400

    fix bug04

--- src/main/java/com/amigoscode/bughunt/easy/bug04/Percentage.java
+++ src/main/java/com/amigoscode/bughunt/easy/bug04/Percentage.java
@@ -3,6 +3,6 @@ package com.amigoscode.bughunt.easy.bug04;
 public class Percentage {
 
     public double percentage(int part, int total) {
-        return (part / total) * 100;
+        return ((double)part / total) * 100;
     }
 }
